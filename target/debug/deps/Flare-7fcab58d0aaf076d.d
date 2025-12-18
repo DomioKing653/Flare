15d88c2c33015bca535dcb373078401bb5f84a2f
Domio <domiomc12@gmail.com>
2025-12-18 13:03:24 +0100

    ...

--- target/debug/deps/Flare-7fcab58d0aaf076d.d
+++
@@ -1,8 +0,0 @@
-C:\Users\simon\RustroverProjects\Flare\target\debug\deps\Flare-7fcab58d0aaf076d.d: src\main.rs src\lexer\mod.rs src\lexer\tokens.rs src\lexer\tokenizer.rs
-
-C:\Users\simon\RustroverProjects\Flare\target\debug\deps\libFlare-7fcab58d0aaf076d.rmeta: src\main.rs src\lexer\mod.rs src\lexer\tokens.rs src\lexer\tokenizer.rs
-
-src\main.rs:
-src\lexer\mod.rs:
-src\lexer\tokens.rs:
-src\lexer\tokenizer.rs:
