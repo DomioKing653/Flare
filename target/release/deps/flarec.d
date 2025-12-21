bc32a23f6e5a58ca86d7b5e116a4213896a02418
Domio <domiomc12@gmail.com>
2025-12-21 15:08:11 +0100

    release 0.0.7

---
+++ target/release/deps/flarec.d
@@ -0,0 +1,5 @@
+C:\Users\simon\RustroverProjects\Flare\target\release\deps\flarec.d: src\bin\flarec.rs
+
+C:\Users\simon\RustroverProjects\Flare\target\release\deps\flarec.exe: src\bin\flarec.rs
+
+src\bin\flarec.rs:
